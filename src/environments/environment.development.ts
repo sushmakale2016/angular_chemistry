7f78ee0f6cc1fbec101138ba230d2075700d176c
sushmakale2016 <sushmankale@gmail.com>
2024-10-19 13:33:20 +0530

    assets folder

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    apiUrl: 'http://13.201.216.250:8080/',
+//    apiUrl: 'http://localhost:8080/',
+};
+
+// import packageInfo from '../../package.json';
+
+// export const environment = {
+//   appVersion: packageInfo.version,
+//   apiUrl: 'http://122.160.30.162/backend/mps/public/v1/api/',
+//   production: true,
+
+// };
---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+// import packageInfo from '../../package.json';
+
+
+// export const environment = {
+//   appVersion: packageInfo.version,
+//   production: true,
+//   // apiUrl: 'http://122.160.30.162/backend/mps/public/v1/api/'
+//   apiUrl: 'http://192.168.1.4/backend/mps/public/v1/api/',
+// };
+
+
+export const environment = {
+    //  apiUrl: 'http://localhost:8080/',
+    apiUrl:'http://13.201.216.250:8080/',
+};
